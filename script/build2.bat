cb64bd375f14bb7628c590b80527f2f510791faa
shownb <micoach@19cn.com>
2022-05-23 12:26:25 +0000

    Update build2.bat

--- script/build2.bat
+++ script/build2.bat
@@ -1,1 +1,11 @@
-##
+@echo off
+
+echo Prepare directories...
+set script_dir=%~dp0
+set src_dir=%script_dir%..
+set build_dir=%script_dir%..\build
+mkdir "%build_dir%"
+
+go get github.com/polevpn/webview
+go mod init view
+go build -ldflags="-H windowsgui" -o webview-example.exe
