18361b545682023c95b2a4c2faaf077885707ab5
shownb <micoach@19cn.com>
2022-05-23 12:34:13 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/polevpn/webview"
 
 func main() {
-	debug := true
-	w := webview.New(false,true)
+
+	w := webview.New(800, 600,false,true)
 	defer w.Destroy()
 	w.SetTitle("Minimal webview example")
 	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+	w.Navigate("https://www.baidu.com")
 	w.Run()
 }
